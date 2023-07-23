a54b15cf14bcae0374c0d08ab5b98439e8d601db
Pranesh-FocusCraft <96162409+Pranesh-FocusCraft@users.noreply.github.com>
2023-07-23 17:27:54 +0000

    1. settings -> files sxcluded 2. added event id for individual 3. added event details files

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../data/dummy-data'
+
 const EventId = () => {
+	const router = useRouter()
+
+	const eventData = getEventById(router.query.eventId)
+
+	if (!eventData) return <h1>No Event found!</h1>
+
 	return (
 		<div>
-			<h1>Events with id</h1>
+			<h1>Events with {router.query.eventId}</h1>
 		</div>
 	)
 }

439852fd7fc6ce570641d45310d974b614b5f7a9
Pranesh-FocusCraft <96162409+Pranesh-FocusCraft@users.noreply.github.com>
2023-07-24 17:53:57 +0000

    added server site fetch code

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+import fs from 'fs/promises'
+import path from 'path'
+
+function HomePage({ products }) {
+	return (
+		<ul>
+			{products.map((a) => (
+				<li key={a.id}>
+					<div>id - {a.id} </div>
+					<div>title - {a.title}</div>
+				</li>
+			))}
+		</ul>
+	)
 }
 
-export default HomePage;
+export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+	const jsonData = await fs.readFile(filePath)
+	const { products } = JSON.parse(jsonData)
+
+	return { props: { products } }
+}
+
+export default HomePage

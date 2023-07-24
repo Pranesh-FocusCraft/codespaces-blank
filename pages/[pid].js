b11b97bbc19cafe72596c28203e8bed49a8fc46a
Pranesh-FocusCraft <96162409+Pranesh-FocusCraft@users.noreply.github.com>
2023-07-24 18:41:55 +0000

    1. added static props return properties 2. added static props function params

---
+++ pages/[pid].js
@@ -0,0 +1,29 @@
+import fs from 'fs/promises'
+import path from 'path'
+
+function ProductDetailPage({ product: { title, description } }) {
+	return (
+		<>
+			<h1> Title - {title} </h1>
+			<p> Description - {description} </p>
+		</>
+	)
+}
+
+export async function getStaticProps({ params }) {
+	const productId = params.pid
+
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+	const jsonData = await fs.readFile(filePath)
+	const { products } = JSON.parse(jsonData)
+
+	const product = products.find((a) => a.id === productId)
+
+	if (!product) {
+		return { notFound: true }
+	}
+
+	return { props: { loadedProduct: product } }
+}
+
+export default ProductDetailPage
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,39 @@
 import fs from 'fs/promises'
+import Link from 'next/link'
 import path from 'path'
 
 function HomePage({ products }) {
 	return (
 		<ul>
-			{products.map((a) => (
-				<li key={a.id}>
-					<div>id - {a.id} </div>
-					<div>title - {a.title}</div>
+			{products.map(({ id, title }) => (
+				<li key={id}>
+					<Link href={`/${id}`}> {title} </Link>
 				</li>
 			))}
 		</ul>
 	)
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+	console.log('data - fetch')
 	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
 	const jsonData = await fs.readFile(filePath)
 	const { products } = JSON.parse(jsonData)
 
-	return { props: { products } }
+	if (!products.length) {
+		return {
+			notFound: true,
+			redirect: {
+				// path if u want to redirect to another page
+				destination: '/no-data-path',
+			},
+		}
+	}
+
+	return {
+		props: { products },
+		revalidate: 10,
+	}
 }
 
 export default HomePage
